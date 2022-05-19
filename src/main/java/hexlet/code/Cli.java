8bf5992231c4fe61d4c2b335c7efe99c873b530f
Aljustal <orbit.fsk@yandex.ru>
2022-05-19 13:15:34 +0300

    fixed Cli.java

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 
 public class Cli {
     public static void greeting() {
-
+        System.out.println();
+        System.out.println("Welcome to the Brain Games dude!");
         System.out.print("May I have your name? ");
         Scanner name = new Scanner(System.in);
-        //String name = s.nextLine();
-        System.out.println("Hello, " + name.nextLine() + "!");
+        String userName = name.nextLine();
+        System.out.println("Hello, " + userName + "!");
     }
 }
