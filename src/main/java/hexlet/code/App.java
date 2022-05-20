d8ae8ee4aab39434eb212b7b1aa1feb94ed64d3d
Aljustal <orbit.fsk@yandex.ru>
2022-05-20 14:49:58 +0300

    fixed App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,27 +1,30 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        //Even.startGame();
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         Scanner choice = new Scanner(System.in);
 
-        int userChoice = choice.nextInt();
+        String userChoice = choice.nextLine();
 
         switch (userChoice) {
-            case 1: Cli.greeting();
+            case "1": Cli.greeting();
                 break;
-            case 2: Even.startGame();
+            case "2": Even.startGame();
                 break;
-            default:
-
+            case "3": Calc.startGame();
+                break;
+            default: System.out.println("You entered the wrong number");
         }
-
     }
 }
