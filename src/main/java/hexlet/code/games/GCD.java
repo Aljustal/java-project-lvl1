6103729190b13b430e2d9ea5e8f9af0f60125dc0
Aljustal <orbit.fsk@yandex.ru>
2022-05-20 15:29:11 +0300

    create game GCD.java

---
+++ src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,44 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void startGame() {
+        String userName = Engine.greetingUser();
+
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+        int correctAnswersCount = 0;
+        while (correctAnswersCount < Engine.GAMESCORETOWIN) {
+            int randomNum1 = Engine.getRandomNum(Engine.MINRANDOM, Engine.MAXRANDOM);
+            int randomNum2 = Engine.getRandomNum(Engine.MINRANDOM, Engine.MAXRANDOM);
+
+            System.out.println("Question: " + randomNum1 + " " + randomNum2);
+            System.out.print("Your answer: ");
+            Scanner userAnswerScanner = new Scanner(System.in);
+            int userAnswer = userAnswerScanner.nextInt();
+            int correctAnswer = gcd(randomNum1, randomNum2);
+
+            if (userAnswer == correctAnswer) {
+                System.out.println("Correct!");
+                correctAnswersCount++;
+            } else {
+                String message = " is wrong answer ;(. Correct answer was ";
+                System.out.println("'" + userAnswer + "'" + message + correctAnswer + ".");
+                break;
+            }
+            if (correctAnswersCount == Engine.GAMESCORETOWIN) {
+                Engine.conratulationUser(userName);
+            }
+        }
+    }
+
+    public static int gcd(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return gcd(num2, num1 % num2);
+    }
+}
