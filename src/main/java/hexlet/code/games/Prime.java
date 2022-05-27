10a166893b54c7776cc469c8d287018e89b8e4e3
Aljustal <orbit.fsk@yandex.ru>
2022-05-27 14:45:22 +0300

    refactoring code

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -23,7 +23,7 @@ public class Prime {
     }
     public static boolean isPrimeNumber(int num) {
         boolean isPrime = true;
-        for (var i = 2; i < Math.sqrt(num); i++) {
+        for (var i = 2; i < num - 1; i++) {
             if (num % i == 0) {
                 isPrime = false;
             }
